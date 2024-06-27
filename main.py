a22f2dd2bde218411855448f68d0db9d8d8f177b
FazinHan - fkt.physik <fizaan.khan@tu-dortmund.de>
2024-06-27 15:23:30 +0200

    update

--- main.py
+++ main.py
@@ -7,4 +7,6 @@ from information import determine_next_filename
 B_arr = np.linspace(0,10,8)
 
 with ProcessPoolExecutor(4) as exe:
-    exe.map(lambda B: os.system(f'python optimiser.py {B}'), B_arr)
+    mapper = exe.map(lambda B: os.system(f'python optimiser.py {B}'), B_arr)
+
+_ = [i for i in mapper]
