03a0ba94c131a7d2a4e1402dc633f6a0c80566d6
FazinHan - fkt.physik <fizaan.khan@tu-dortmund.de>
2024-06-27 15:24:16 +0200

    update

--- main.py
+++ main.py
@@ -9,4 +9,5 @@ B_arr = np.linspace(0,10,8)
 with ProcessPoolExecutor(4) as exe:
     mapper = exe.map(lambda B: os.system(f'python optimiser.py {B}'), B_arr)
 
-_ = [i for i in mapper]
+for i in mapper:
+    print(i)
