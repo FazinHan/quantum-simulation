24b476cc99a97d53d03c3d31104a59bd518211e9
FazinHan - fkt.physik <fizaan.khan@tu-dortmund.de>
2024-07-03 18:40:30 +0200

    update

--- main.py
+++ main.py
@@ -32,6 +32,6 @@ if __name__=="__main__":
     fig.suptitle(f'$\\Omega={omega}$, $J={J}$, $J_{{||}}={JII}$')
     plt.savefig(determine_next_filename(folder='outputs'))
 
-    # qiskit_cost_plotter(ls, costs, omega, J, JII)
-    qiskit_penalty_plotter(ls, penalties, omega, J, JII)
+    qiskit_cost_plotter(ls, costs, omega, J, JII)
+    # qiskit_penalty_plotter(ls, penalties, omega, J, JII)
     plt.savefig(determine_next_filename(folder='outputs'))
