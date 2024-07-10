7d1925b818c7aa6159a9ff0c446046d60a7cb9b0
FazinHan - fkt.physik <fizaan.khan@tu-dortmund.de>
2024-07-10 17:11:27 +0200

    update

--- julia_call.py
+++ julia_call.py
@@ -35,6 +35,6 @@ if __name__=="__main__":
     #     file.write(str(errors.to_list()))
     
     print('complete')
-    print('pushed successfully')
     os.system("git commit -am 'update'")
     os.system("git push")
+    print('pushed successfully')
