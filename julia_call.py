7e0563e4b9dd763a8030f224a89a9df5936cc4b9
FazinHan - fkt.physik <fizaan.khan@tu-dortmund.de>
2024-07-10 17:05:27 +0200

    update

--- julia_call.py
+++ julia_call.py
@@ -2,12 +2,14 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 from concurrent.futures import ProcessPoolExecutor
+from information import determine_next_filename
 
 '''$(i) A$(i_A)_length$(chain_length)_layer_plan$(layer_plan) $(timeevo) $(en)'''
 
 chain_length = 4
 
 layer_plan = [4,5,[2,1,1,2]] # if i is int, circuit will have i type 2 (complete) layers
+# layer_plan = [1,2]
 
 errors = []
 
@@ -26,8 +28,10 @@ if __name__=="__main__":
 
     with ProcessPoolExecutor(3) as exe:
         a = [0 for _ in exe.map(func, layer_plan)]
-    
-    with open('julia_result.out','wb') as file:
-        np.savez(file, layer_plan=layer_plan, errors=errors)
+
+    name = determine_next_filename('julia_result_errors','txt','data')
+    with open(name,'w') as file:
+        file.write(str(layer_plan))
+        file.write(str(errors.to_list()))
     
     print('complete')
