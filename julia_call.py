dd36c1768f401868478ca5f58806452a0d25264f
FazinHan - fkt.physik <fizaan.khan@tu-dortmund.de>
2024-07-11 16:26:32 +0200

    update

--- julia_call.py
+++ julia_call.py
@@ -6,12 +6,11 @@ from information import determine_next_filename
 
 '''$(i) A$(i_A)_length$(chain_length)_layer_plan$(layer_plan) $(timeevo) $(en)'''
 
-chain_length = 2
-JII = np.arange(0,1,.1)
+chain_length = 4
 
-layer_plan = [2,[2,1]] # if i is int, circuit will have i type 2 (complete) layers
+layer_plan = [3,4,[2,1,1]] # if i is int, circuit will have i type 2 (complete) layers
 # layer_plan = [1,2]
-Jii = 1
+Jii = float(sys.argv[1])
 B = range(1,11)
 
 if __name__=="__main__":
@@ -35,9 +34,9 @@ if __name__=="__main__":
 
     # print(errors)
 
-    name = determine_next_filename('julia_result_errors','txt','data')
+    name = determine_next_filename(f'julia_result_errors{Jii}','txt','data')
     with open(name,'a+') as file:
-        file.write(str(layer_plan)+'\n'+str(errors))
+        file.write(str(layer_plan)+'\n'+list(B)+'\n'+str(errors))
         print('written to',name)
     
     print(f'\ncomplete in {np.round(t1-t0,1)}s\n')
---
+++ parallel_julia.py
@@ -0,0 +1,12 @@
+import sys
+from information import determine_next_filename
+from concurrent.futures import ProcessPoolExecutor
+
+Jii = range(1,11)
+
+def func(jii):
+    os.system(f'python julia_call.py {jii}')
+
+if __name__=="__main__":
+    with ProcessPoolExecutor(len(Jii)) as exe:
+        [0 for _ in exe.map(func, Jii)]
