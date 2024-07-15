bb380e73353d8c167794d4940cde863632dce9a4
FazinHan <freak.fizaan@gmail.com>
2024-07-15 10:55:05 +0200

    fully parallelise

--- parallel_julia.py
+++ parallel_julia.py
@@ -5,9 +5,13 @@ from information import determine_next_filename
 from concurrent.futures import ProcessPoolExecutor
 from physics import B_range
 
+B_vec = np.linspace(*B_range,2)
+
 def func(b):
     os.system(f'python julia_call.py {b}')
 
 if __name__=="__main__":
-    for i in B_range:
-        func(i)
+    # for i in B_vec:
+        # func(i)
+    with ProcessPoolExecutor(B_vec.size) as exe:
+        [_ for _ in exe.map(func, B_vec)]
