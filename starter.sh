90b4c07fbf073392e19d8bc36ca77bdcb6086583
FazinHan <freak.fizaan@gmail.com>
2024-12-12 13:17:32 +0530

    uncomment execution of parallel_julia.py in starter.sh

--- starter.sh
+++ starter.sh
@@ -7,9 +7,4 @@
 #SBATCH -A physics_engg
 #SBATCH --mail-user=fizaan.khan.phy21@iitbhu.ac.in
 
-#source ~/.bashrc
 time mpiexec.hydra -n $SLURM_NTASKS python parallel_julia.py
-#python parallel_julia.py 4
-#python parallel_julia.py 6
-#python parallel_julia.py 8
-#push
