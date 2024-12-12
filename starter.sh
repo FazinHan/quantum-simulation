75e88207b2fd3332c39d8cb77dda07835534d5a7
fizaank.phy21.iitbhu <fizaan.khan.phy21@iitbhu.ac.in>
2024-12-12 13:05:37 +0530

    update

--- starter.sh
+++ starter.sh
@@ -5,7 +5,7 @@
 #SBATCH --time=4-00:00:00
 
 #source ~/.bashrc
-time mpiexec.hydra -n $SLURM_NTASKS python parallel_julia.py
+time mpiexec.hydra -n $SLURM_NTASKS source ~/.bashrc && cd quantum-simulation && python parallel_julia.py
 #python parallel_julia.py 4
 #python parallel_julia.py 6
 #python parallel_julia.py 8
