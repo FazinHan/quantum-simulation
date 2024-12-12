e3777c4021252e04c9b12c1c6b3edc1dbb61d643
fizaank.phy21.iitbhu <fizaan.khan.phy21@iitbhu.ac.in>
2024-12-12 13:09:03 +0530

    update

--- starter.sh
+++ starter.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name=jeff
-#SBATCH --ntasks=2
+#SBATCH -N 1
+#SBATCH --job-name=fauseweh-zhu
 #SBATCH --output=qs.out
 #SBATCH --time=4-00:00:00
+#SBATCH --ntasks-per-node=2
+#SBATCH -A physics_engg
+#SBATCH --mail-user=fizaan.khan.phy21@iitbhu.ac.in
 
 #source ~/.bashrc
-time mpiexec.hydra -n $SLURM_NTASKS source ~/.bashrc && cd quantum-simulation && python parallel_julia.py
+time mpiexec.hydra -n $SLURM_NTASKS python parallel_julia.py
 #python parallel_julia.py 4
 #python parallel_julia.py 6
 #python parallel_julia.py 8
